cb470e70bcbf87442b34d5ba2384c842402c7620
Omar ALQAWASMEH <omar.alqawasmeh@emse.fr>
2020-08-01 11:49:45 +0200

    general menu updates

--- dev.sh
+++ dev.sh
@@ -3,6 +3,5 @@ rm build/sublime/LinkedData.sublime-package
 npx emk
 #cp build/sublime/LinkedData.sublime-package "/home/omar/.config/sublime-text-3/Installed Packages/LinkedData.sublime-package"
 #chmod 777 "/home/omar/.config/sublime-text-3/Installed Packages/LinkedData.sublime-package"'
-
 #cp build/sublime/assets/sparql-generate.sublime-syntax "/home/omar/.config/sublime-text-3/Packages/LinkedData/"
-cp build/sublime/assets/sparql-generate.* "/home/omar/.config/sublime-text-3/Packages/LinkedData/"
+cp build/sublime/assets/* "/home/omar/.config/sublime-text-3/Packages/LinkedData/"
